ae60f35783a7e4cb67e4c147520c94f1d9d2b772
11Carol02 <130381377+11Carol02@users.noreply.github.com>
2024-04-03 16:37:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello PSG students batch 1 team 4!!!!'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
-
